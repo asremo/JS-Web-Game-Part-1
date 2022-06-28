9a09c9b66d46f920ae7e0ee3f73077b7f314f8d5
Amanda Remo <amandaremo@ip-192-168-86-34.ec2.internal>
2022-06-28 01:42:26 -0400

    finished commit

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom) {
+    let picture = document.createElement('img')
+    picture.src = url
+    picture.style.position = 'fixed'
+    picture.style.left = left + 'px'
+    picture.style.bottom = bottom + 'px'
+    document.body.append(picture)
+    return picture
+}
+
+function newItem(url, left, bottom) {
+    let picture = newImage(url, left, bottom)
+
+    picture.addEventListener('dblclick', function(){
+        picture.remove()
+    })
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
+
+
+
